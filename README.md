25164fa7c75b374ed4d58b0a89315105da44f9a9
Hubert Jacquin <hj@taktik.com>
2017-07-14 11:21:11 +0200

    create modules from template

---
+++ README.md
@@ -0,0 +1,35 @@
+# \<taktik-search-api-mixin\>
+
+
+
+## Install the Polymer-CLI
+
+First, make sure you have the [Polymer CLI](https://www.npmjs.com/package/polymer-cli) installed. Then run `polymer serve` to serve your element locally.
+
+## install project dependency
+
+run nmp and bower install.
+```
+$ npm install
+$ bower install
+```
+
+## compile your code
+
+```
+$ node_modules/typescript/bin/tsc
+```
+
+## Viewing Your Element
+
+```
+$ polymer serve
+```
+
+## Running Tests
+
+```
+$ polymer test
+```
+
+Your application is already set up to be tested via [web-component-tester](https://github.com/Polymer/web-component-tester). Run `polymer test` to run your application's test suite locally.
---
+++ taktik-search-api-mixin.d.ts
@@ -0,0 +1,30 @@
+declare function TaktikSearchApiMixin(element: any): TaktikSearchApiMixinConstructor;
+interface TaktikSearchApiMixinConstructor {
+    new (): TaktikSearchApiMixinType;
+}
+/**
+ *
+ * defines standard behavior for search modules compatible with *taktik-free-text-search*.
+ *
+ *
+ * ### Events
+ *
+ * * *results-found* Fired when results are found by the API.
+ *
+ */
+declare class TaktikSearchApiMixinType extends PolymerElement {
+    /**
+     * searchString string for search query.
+     */
+    searchString: string;
+    /**
+     * Array of search results
+     */
+    searchResults: string;
+    _setSearchResults(): void;
+    /**
+     * If true, automatically performs an Ajax request when either *searchString*, *itemType* or *size* changes.
+     */
+    auto: boolean;
+    _resultsFound(): void;
+}
